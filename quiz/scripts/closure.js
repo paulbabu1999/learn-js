6c2d428e968ec97ff7ee622af0ac9f3f4c6077cf
paul babu <paulbabu.1999@gmail.com>
2024-01-24 16:23:51 -0500

    add class activity

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -4,6 +4,7 @@ function setColor(set) {
             let userColor = document.getElementById('color').value;
             document.getElementById('myPara').style.color = userColor;
         }
+        return setColor
         
 }
 
