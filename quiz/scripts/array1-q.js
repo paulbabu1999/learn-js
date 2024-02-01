37f9c2f45aed1c719cfef02e027a30601fd908d8
paul babu <paulbabu.1999@gmail.com>
2024-02-01 17:18:40 -0500

    add inclass changes

--- quiz/scripts/array1-q.js
+++ quiz/scripts/array1-q.js
@@ -8,19 +8,28 @@ window.onload = function() {
     const MAX_HISTORY = 5;
 
     btn.onclick = () => {
+        
     // we will only allow a term to be entered if the search input isn't empty
       if (inp.value !== '') {
               // empty the list so that we don't display duplicate entries
               // the display is regenerated every time a search term is entered.
-              list.innerHTML = '';
+              myHistory.unshift(inp.value);
+              list.innerHTML='';
+              let myHistoryCopy=Array.from(myHistory);
+              myHistoryCopy.sort(function(a,b){return a.length-b.length;});
+
 
               // loop through the sorted array, and display all the search terms in the list
               for (const itemText of myHistoryCopy) {
+                const listItem=document.createElement('li')
+                listItem.textContent=itemText
+                list.append(listItem)
                 
               }
 
               // If the array length is 5 or more, remove the oldest search term
               if (myHistory.length >= MAX_HISTORY) {
+                myHistory.pop()
                 
               }
 
--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -10,22 +10,49 @@ function createTRNode(colNodes) {
   return trNode;
 }
 
-function createTDNode(childNode) {
+function createTDNode(childNode1) {
   let tdNode = document.createElement("td");
-  tdNode.appendChild(childNode);
+  tdNode.appendChild(childNode1);
+
   return tdNode;
 }
+function createTDButton(label,event,listener) {
+  let buttonNode = document.createElement("button");
+  let buttonLabel= document.createTextNode(label);
+  buttonNode.appendChild(buttonLabel)
+  if (listener){
+  buttonNode.addEventListener(event,listener);
+  }
+  return buttonNode;
+}
+function editbox(parentNode){
+  var element=document.createElement("input");
+  element.type="text";
+  element.placeholder="Enter Cell (x,y) ..."
+  var place = document.createTextNode("Label");
+  parentNode.replaceChild(element,parentNode.childNodes[0])
+
+  
+  
+}
+
+
 
 function createTxtNode(txt) {
   let txtNode = document.createTextNode(txt);
   return txtNode;
 }
 
+
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2= createTDNode(createTDButton("Edit Text","click",()=>
+      editbox(col1)
+    ))
+    tableNode.appendChild(createTRNode([col1,col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
