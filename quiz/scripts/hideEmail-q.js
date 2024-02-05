a5101e2c6096ca06942fe31289b6b6bb9ad4b209
paul babu <paulbabu.1999@gmail.com>
2024-02-04 23:30:03 -0500

    Revert "to hide email"
    This reverts commit 58f14a14f11e9bc17b488fb734560dddd8153c1a.

--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -1,21 +1,11 @@
-/* anonymizes the username(local part) of an email ID, that is,
+/* anonymizes the username(local part) of an email ID, that is, 
 replace the username part with * characters.
 */
-const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.edu', 'jk@neu.edu', 'jsned@', 'ai_me@mugar.northeastern.edu',"pa"];
+const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.edu', 'jk@neu.edu', 'jsned@', 'ai_me@mugar.northeastern.edu'];
 window.onload = function hideEmail() {
     const list = document.getElementById('emails')
     list.innerHTML = '';
     for (const email of emails) {
-
-        if(email.match(/^[a-zA-Z0-9_]+@(.+\.)*northeastern.edu$/)){
-           console.log(email)
-           const listItem=document.createElement('li');
-           maskedEmail = email.replace(/^[^@]+/, match => '*'.repeat(match.length));
-           listItem.textContent = maskedEmail;
-           list.appendChild(listItem);
-
-        }
+        // complete the loop
     }
-
-
 }
